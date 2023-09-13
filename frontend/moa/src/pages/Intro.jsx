2427963e3b12e42641e3d25796d8748b319b46fa
이진형 <wlsgud4084@naver.com>
2023-09-13 16:35:10 +0900

    comment: temporary main page button #6
    메인페이지 이동 버튼 관련 주석 추가

--- frontend/moa/src/pages/Intro.jsx
+++ frontend/moa/src/pages/Intro.jsx
@@ -15,7 +15,10 @@ function Intro(props) {
           <button>회원가입</button>
         </Link>
       </div>
+
       <hr />
+      
+      {/* 메인페이지 이동을 위해 임시로 만든 버튼 */}
       <div>
         <Link to="/">
           <button>메인페이지로 이동</button>
