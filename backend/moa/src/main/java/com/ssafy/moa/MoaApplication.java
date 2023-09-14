b890ebb96937f80f1a7ffddedeaa768564db0c2d
hyoinjeong <hyoin.jeong3@gmail.com>
2023-09-14 10:43:48 +0900

    Chore: develop push 테스트

--- backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
+++ backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MoaApplication {
 
+    // main
     public static void main(String[] args) {
         SpringApplication.run(MoaApplication.class, args);
     }
