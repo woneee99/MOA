4f5c8132729c838e8b70db6e2528de0a15df71ac
박진희 <wlsl0620@naver.com>
2023-09-14 11:56:51 +0900

    Feat : 회원가입 페이지 배치 #8

--- frontend/moa/src/pages/SignUp.jsx
+++ frontend/moa/src/pages/SignUp.jsx
@@ -5,6 +5,43 @@ function SignUp(props) {
   return (
     <div>
       <p>SignUp Page</p>
+      <div className='signUpContainer'>
+        <div>
+          <label htmlFor="password" className="inputTitle">나는</label>
+          <input type="radio" id="" name=""/>외국인
+          <input type="radio" id="" name=""/>한국인
+        </div>
+
+        <div className="inputForm">
+          <label htmlFor="password" className="inputTitle">이름</label>
+          <input type="text" id="" name=""/>
+        </div>
+
+        <div className="inputForm">
+          <label htmlFor="password" className="inputTitle">출신국가</label>
+          <input type="text" id="" name=""/>
+        </div>
+
+        <div className="inputForm">
+          <label htmlFor="password" className="inputTitle">성별</label>
+          <input type="radio" id="" name=""/>남자
+          <input type="radio" id="" name=""/>여자
+        </div>
+
+        <div className="inputForm">
+          <label htmlFor="password" className="inputTitle">이메일</label>
+          <input type="text" id="" name=""/>
+        </div>
+
+        <button>인증번호 전송</button>
+
+        <div className="inputForm">
+          <label htmlFor="password" className="inputTitle">비밀번호</label>
+          <input type="password" id="" name=""/>
+        </div>
+
+
+      </div>
       <BackButton />
     </div>
   );
