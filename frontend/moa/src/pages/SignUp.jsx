e615800f51f8a9cbe70434eb2b4ac88e87b5eca5
박진희 <wlsl0620@naver.com>
2023-09-14 12:16:47 +0900

    Feat : 페이지 이동 공통 메인버튼 제작 #8

--- frontend/moa/src/pages/SignUp.jsx
+++ frontend/moa/src/pages/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import BackButton from '../components/BackButton';
+import MainButton from '../components/MainButton';
 
 function SignUp(props) {
   return (
@@ -7,39 +8,34 @@ function SignUp(props) {
       <p>SignUp Page</p>
       <div className='signUpContainer'>
         <div>
-          <label htmlFor="password" className="inputTitle">나는</label>
+          <label htmlFor="" className="inputTitle">나는</label>
           <input type="radio" id="foreigner" name="foreigner"/>외국인
           <input type="radio" id="korean" name="korean"/>한국인
         </div>
 
         <div className="inputForm">
-          <label htmlFor="password" className="inputTitle">이름</label>
+          <label htmlFor="name" className="inputTitle">이름</label>
           <input type="text" id="name" name="name"/>
         </div>
 
         <div className="inputForm">
-          <label htmlFor="password" className="inputTitle">국적</label>
+          <label htmlFor="region" className="inputTitle">국적</label>
           <select name="region">
             <option value="한국">한국</option>
             <option value="미국">미국</option>
           </select>
         </div>
-        {/* <div className="inputForm">
-          <label htmlFor="password" className="inputTitle">국적</label>
-          <input type="text" id="region" name="region"/>
-        </div> */}
 
         <div className="inputForm">
-          <label htmlFor="password" className="inputTitle">성별</label>
+          <label htmlFor="gender" className="inputTitle">성별</label>
           <input type="radio" id="man" name="man"/>남자
           <input type="radio" id="woman" name="woman"/>여자
         </div>
 
         <div className="inputForm">
-          <label htmlFor="password" className="inputTitle">이메일</label>
+          <label htmlFor="email" className="inputTitle">이메일</label>
           <input type="text" id="email" name="email"/>
         </div>
-
         <button>인증번호 전송</button>
 
         <div className="inputForm">
@@ -47,8 +43,9 @@ function SignUp(props) {
           <input type="password" id="password" name="password"/>
         </div>
 
-
+        <MainButton text="회원가입" to="/intro" />
       </div>
+      
       <BackButton />
     </div>
   );
