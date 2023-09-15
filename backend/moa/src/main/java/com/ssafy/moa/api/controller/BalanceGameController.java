b24fa1ac922c0760991d7d8d08e441ab2ab95667
hyoinjeong <hyoin.jeong3@gmail.com>
2023-09-15 09:25:30 +0900

    Fix: 밸런스게임 등록 API swagger 추가 #3

--- backend/moa/src/main/java/com/ssafy/moa/api/controller/BalanceGameController.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/controller/BalanceGameController.java
@@ -5,6 +5,7 @@ import com.ssafy.moa.api.jwt.JwtTokenProvider;
 import com.ssafy.moa.api.service.BalanceGameService;
 import com.ssafy.moa.common.utils.ApiUtils;
 import com.ssafy.moa.common.utils.ApiUtils.ApiResult;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -19,6 +20,7 @@ public class BalanceGameController {
     private final BalanceGameService balanceGameService;
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Operation(summary = "밸런스게임 등록", description = "밸런스게임을 등록할 수 있습니다.", tags = { "BalanceGame Controller" })
     // Todo : 추후 authentication를 사용해서 실제 memberId로 변경
     @PostMapping
     public ApiResult<Long> createBalanceGame(/*@RequestHeader("Authorization") String header,*/ @RequestBody BalanceGameDto balanceGameDto){
