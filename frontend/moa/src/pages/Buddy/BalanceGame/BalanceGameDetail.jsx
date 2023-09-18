4390e4268f51e1315ae2d9b23db00acfb002b020
이진형 <wlsgud4084@naver.com>
2023-09-18 12:10:53 +0900

    rename: edit repository name and move files #17
    버디 폴더명 변경 및 파일 이동

--- frontend/moa/src/pages/Buddy/BalanceGame/BalanceGameDetail.jsx
+++ frontend/moa/src/pages/Buddy/BalanceGame/BalanceGameDetail.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router';
 import { useLocation, Link } from 'react-router-dom';
 
-import BackButton from '../../components/BackButton';
+import BackButton from '../../../components/BackButton';
 
 function BalanceGameDetail(props) {
   const location = useLocation();
