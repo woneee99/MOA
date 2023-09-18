7b631ed84e0100340e41481fd2e8c219de9890a0
박진희 <wlsl0620@naver.com>
2023-09-18 14:31:45 +0900

    Feat : KoreanBuddy Component 추가

---
+++ frontend/moa/src/components/Matching/KoreanBuddy.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function KoreanBuddy(){
+  return(
+    <>
+      <h3>선호하는 버디의 국적을 알려주세요</h3>
+      <select>
+        <option value="미국">미국</option>
+        <option value="일본">일본</option>
+        <option value="중국">중국</option>
+        <option value="영국">영국</option>
+      </select>
+      <p>에서 온 버디를 만나볼래요!</p>
+    </>
+  );
+};
+
+export default KoreanBuddy;
