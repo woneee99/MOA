0faec744bd3a1000ff0471ef72b132570c7d398f
황주원 <woneee99@naver.com>
2023-09-19 11:24:30 +0900

    Fix: CORS error

--- backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
+++ backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
@@ -19,7 +19,7 @@ public class MoaApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://moamore.site/");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
