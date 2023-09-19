7f76093bae1a21c0c75c44bba65094345bcef243
이진형 <wlsgud4084@naver.com>
2023-09-19 10:53:43 +0900

    feat: balance game api #17
    밸런스게임 관련 api 파일 작성

--- frontend/moa/src/pages/Buddy/BalanceGame/BalanceGame.jsx
+++ frontend/moa/src/pages/Buddy/BalanceGame/BalanceGame.jsx
@@ -2,57 +2,26 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
 
+import { balanceGameApi } from '../../../api/balanceGameApi';
+
 import BackButton from '../../../components/BackButton';
 import BalanceGameItem from '../../../components/BalanceGame/BalanceGameItem';
 import PopularBalanceGameItem from '../../../components/PopularBalanceGameItem';
 
 function BalanceGame(props) {
-  const [balanceGames, setBalanceGames] = useState([
-    { id : 1, title: '제목1', username: '유저1', time: 30,
-      balanceGameList : [
-        { order : 1, left : 'A', right : 'B' },
-        { order : 2, left : 'C', right : 'D' },
-        { order : 3, left : 'E', right : 'F' },
-        { order : 4, left : 'G', right : 'H' },
-        { order : 5, left : 'I', right : 'J' },
-      ],
-    },
-    { id : 2, title: '제목2', username: '유저2', time: 60,
-      balanceGameList : [
-        { order : 1, left : 'ㄱ', right : 'ㄴ' },
-        { order : 2, left : 'ㄷ', right : 'ㄹ' },
-        { order : 3, left : 'ㅁ', right : 'ㅂ' },
-        { order : 4, left : 'ㅅ', right : 'ㅇ' },
-        { order : 5, left : 'ㅈ', right : 'ㅊ' },
-      ],
-    },
-    { id : 3, title: '제목3', username: '유저3', time: 90,
-      balanceGameList : [
-        { order : 1, left : 'ㄱ', right : 'ㄴ' },
-        { order : 2, left : 'ㄷ', right : 'ㄹ' },
-        { order : 3, left : 'ㅁ', right : 'ㅂ' },
-      ],
-    },
-  ]);
+  const [balanceGames, setBalanceGames] = useState([]);
+  const [popularBalanceGames, setPopularBalanceGames] = useState([]);
 
-  const [popularBalanceGames, setPopularBalanceGames] = useState([
-    { id : 1, title: '제목1', username: '유저1', time: 30,
-      balanceGameList : [
-        { order : 1, left : 'A', right : 'B' },
-        { order : 2, left : 'C', right : 'D' },
-        { order : 3, left : 'E', right : 'F' },
-        { order : 4, left : 'G', right : 'H' },
-        { order : 5, left : 'I', right : 'J' },
-      ],
-    },
-    { id : 3, title: '제목3', username: '유저3', time: 90,
-      balanceGameList : [
-        { order : 1, left : 'ㄱ', right : 'ㄴ' },
-        { order : 2, left : 'ㄷ', right : 'ㄹ' },
-        { order : 3, left : 'ㅁ', right : 'ㅂ' },
-      ],
-    },
-  ]);
+  useEffect(() => {
+    balanceGameApi.getBalanceGameList()
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  })
+
 
   const navigate = useNavigate();
 
