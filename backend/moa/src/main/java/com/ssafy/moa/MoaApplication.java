a1553073bc9604a5de82d4ee754882cae5ae65ea
이하린 <gmlwls608@gmail.com>
2023-09-19 17:31:18 +0900

    Fix: Cors Error resolve

--- backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
+++ backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
@@ -19,7 +19,12 @@ public class MoaApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
+                                        HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
+                                        HttpMethod.PATCH.name())
+                        .maxAge(1800);
             }
         };
     }
