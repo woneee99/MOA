14cb663d3be27bbaab208e5672ba6a5f1e27e933
황주원 <woneee99@gmail.com>
2023-09-19 11:10:24 +0900

    Feat: CORS

--- backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
+++ backend/moa/src/main/java/com/ssafy/moa/MoaApplication.java
@@ -2,7 +2,9 @@ package com.ssafy.moa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MoaApplication {
@@ -10,6 +12,16 @@ public class MoaApplication {
     // main
     public static void main(String[] args) {
         SpringApplication.run(MoaApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://moamore.site/");
+            }
+        };
     }
 
 }
