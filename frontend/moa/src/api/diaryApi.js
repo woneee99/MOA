eea496d02e3a4820993946f0334836783ef98df8
이진형 <wlsgud4084@naver.com>
2023-09-19 15:27:34 +0900

    feat: add diary apis #24
    교환일기 관련 api 추가

--- frontend/moa/src/api/diaryApi.js
+++ frontend/moa/src/api/diaryApi.js
@@ -1,5 +1,18 @@
 import { axios } from './https';
 
 export const diaryApi = {
+  // 교환일기 전체 조회
+  getDiaryList : () => axios.get('diary'),
 
+  // 교환일기 상세 조회
+  getDiaryDetail : (exchangeDiaryId) => axios.get(`diary/${exchangeDiaryId}`),
+
+  // 교환일기 생성
+  createDiary : (data) => axios.post('diary', data),
+
+  // 교환일기 수정
+  updateDiary : (exchangeDiaryId, data) => axios.put(`diary/${exchangeDiaryId}`, data),
+
+  // 교환일기 삭제
+  deleteDiary : (exchangeDiaryId) => axios.delete(`diary/${exchangeDiaryId}`),
 }
