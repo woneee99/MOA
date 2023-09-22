7b35295de10db4e58e3ae6b7b1df58b53da84787
이진형 <wlsgud4084@naver.com>
2023-09-22 17:53:45 +0900

    fix: matching page #20
    매칭 페이지 수정

--- frontend/moa/src/pages/Matching.jsx
+++ frontend/moa/src/pages/Matching.jsx
@@ -7,23 +7,43 @@ import BuddyGender from "../components/Matching/BuddyGender";
 import LoadingMatching from "../components/Matching/LoadingMatching";
 import SuccessMatching from "../components/Matching/SuccessMatching";
 import KoreanBuddy from "../components/Matching/KoreanBuddy";
-import { userApi } from '../api/userApi';
 
-function Matching(){
-  return(
+function Matching() {
+  // const location = useLocation();
+  // const foreigner = location.state.isForeigner;
+
+  // console.log(foreigner);
+
+  const [isForeigner, setIsForeigner] = useState(false);
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const steps = [
+    isForeigner ? (
+      <h3>반가워요 멋진 한국인 친구, 버디를 만나보세요</h3>
+    ) : (
+      <h3>한국을 좋아하는 외국인 친구의 버디가 되어보세요</h3>
+    ),
+    !isForeigner ? <KoreanBuddy /> : null,
+    <BuddyInterest />,
+    <BuddyGender />,
+    <LoadingMatching />,
+    <SuccessMatching />,
+  ];
+
+  useEffect(() => {
+    // Your useEffect logic here
+  }, []);
+
+  return (
     <>
       <div>매칭 페이지</div>
       {/* Display the current step */}
-      {isForeigner ? (
-        <h3>반가워요 멋진 한국인 친구, 버디를 만나보세요</h3>
-      ) : (
-        <h3>한국을 좋아하는 외국인 친구의 버디가 되어보세요</h3>
-      )}
-      <ConfirmButton text="시작하기" onClick={() => !isForeigner? setCurrentStep(0):setCurrentStep(1)} />
+      {steps[currentStep]}
+      <ConfirmButton text="시작하기" onClick={() => setCurrentStep(currentStep + 1)} />
 
       {/* Display the "Next" button if not on the last step */}
-      {currentStep < steps.length - 1 && (
-        <button onClick={() => setCurrentStep(currentStep + 1)}>다음 단계</button>
+      {currentStep > 0 && currentStep < steps.length - 1 && (
+        <button onClick={() => setCurrentStep(currentStep + 1)}>Next</button>
       )}
     </>
   );
