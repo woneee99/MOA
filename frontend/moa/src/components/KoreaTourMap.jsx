a9f3b7b5731a4c6ee3a46578f07e84f84bb378e0
hyoinjeong <hyoin.jeong3@gmail.com>
2023-09-22 16:19:05 +0900

    Feat: KakaoMap 연동 #29

--- frontend/moa/src/components/KoreaTourMap.jsx
+++ frontend/moa/src/components/KoreaTourMap.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 function KoreaTourMap(props) {
+  const locations = [
+		{ title: '카카오', latlng: { lat: 33.450705, lng: 126.570677 } },
+		{ title: '생태연못', latlng: { lat: 33.450936, lng: 126.569477 } },
+		{ title: '텃밭', latlng: { lat: 33.450879, lng: 126.56994 } },
+		{ title: '근린공원', latlng: { lat: 33.451393, lng: 126.570738 } },
+  ];
+  
   return (
-    <div>
-      <h1>대충 지도 Component</h1>
-    </div>
+    <Map center={{ lat: 33.450701, lng: 126.570667 }} style={{ width: '800px', height: '600px' }} level={3}>
+			{locations.map((loc, idx) => (
+				<MapMarker
+					key={`${loc.title}-${loc.latlng}`}
+					position={loc.latlng}
+					image={{
+						src: 'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png',
+						size: { width: 24, height: 35 },
+					}}
+					title={loc.title}
+				/>
+			))}
+		</Map>
   );
 }
+
 
 export default KoreaTourMap;
