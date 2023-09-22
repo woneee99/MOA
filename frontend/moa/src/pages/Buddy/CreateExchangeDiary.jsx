2449721c641ee2ee32bac5da2237fb39dbc8ef97
이진형 <wlsgud4084@naver.com>
2023-09-22 15:57:56 +0900

    feat: diary #24
    교환일기 api 적용 대비 자리 마련

--- frontend/moa/src/pages/Buddy/CreateExchangeDiary.jsx
+++ frontend/moa/src/pages/Buddy/CreateExchangeDiary.jsx
@@ -1,25 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { diaryApi } from '../../api/diaryApi';
+
+import { useNavigate } from 'react-router-dom';
 
 import BackButton from '../../components/BackButton';
 
 function CreateExchangeDiary(props) {
+  const [exchangeDiaryContent, setExchangeDiaryContent] = useState('');
+  const [imageFile, setImageFile] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleContentChange = (e) => {
+    setExchangeDiaryContent(e.target.value);
+  };
+  const handleImageChange = (e) => {
+    setImageFile(e.target.files[0]); // 이미지 파일 설정
+  };
+
+  const createDiary = () => {
+    const exchangeDiaryRequest = {
+      exchangeDiaryContent: exchangeDiaryContent,
+    };
+
+    const formData = new FormData();
+    const jsonBlob = new Blob([JSON.stringify(exchangeDiaryRequest)], { type: "application/json" });
+
+    formData.append('saveOpenChatRequest', jsonBlob);
+    formData.append('multipartFile', imageFile);
+
+    const headers = {
+      'Content-Type': 'multipart/form-data',
+    };
+
+    diaryApi.createDiary(formData, { headers })
+    .then((response) => {
+      navigate('/buddy/exchangediary');
+    })
+    .catch((error) => {
+      console.log('교환일기 생성 에러 발생');
+      console.log(error);
+    })
+
+
+  }
   return (
     <div>
       <BackButton />
       <h1>교환일기 작성</h1>
       <div>
-        <p>사진 업로드 Component</p>
+        <label htmlFor="diaryImage">이미지 업로드</label>
+        <input
+          type="file"
+          id="diaryImage"
+          accept="image/*"
+          onChange={handleImageChange}
+        />
       </div>
       <div>
-        <label htmlFor="title">제목</label>
-        <input type="text" id="title" />
-      </div>
-      <div>
-        <label htmlFor="content">내용</label>
-        <input type="text" id="content" />
+        <label htmlFor="diaryContent">설명</label>
+        <input
+          type="text"
+          id="diaryContent"
+          value={exchangeDiaryContent}
+          onChange={handleContentChange}
+        />
       </div>
       <hr />
-      <button>생성하기</button>
+      <button onClick={createDiary}>생성하기</button>
     </div>
   );
 }
