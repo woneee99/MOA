97c7e8657c4113f7ca2c8fb52b5f026ee95c1d40
이하린 <gmlwls608@gmail.com>
2023-09-22 16:43:02 +0900

    Chore: 퀴즈 제출 api 하나로 통합 #18

--- backend/moa/src/main/java/com/ssafy/moa/api/controller/QuizController.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/controller/QuizController.java
@@ -5,6 +5,7 @@ import com.ssafy.moa.api.jwt.JwtTokenProvider;
 import com.ssafy.moa.api.service.QuizService;
 import com.ssafy.moa.common.utils.ApiUtils;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ import static com.ssafy.moa.common.utils.ApiUtils.success;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "quiz", description = "퀴즈 API")
 @RequestMapping("/quiz")
 public class QuizController {
 
@@ -25,18 +27,10 @@ public class QuizController {
 
     // 단어 퀴즈 출제 API
     @GetMapping("/word")
-    @Operation(summary = "단어 퀴즈 출제 API", description = "단어 퀴즈를 랜덤으로 15개 출제하는 API입니다.")
+    @Operation(summary = "단어 퀴즈 출제", description = "단어 퀴즈를 랜덤으로 15개 출제하는 API입니다.")
     public ApiResult<List<QuizQuestionDto>> questionWordQuiz(){
         List<QuizQuestionDto> quizQuestionDtoList = quizService.questionWordQuiz();
         return success(quizQuestionDtoList);
-    }
-
-    // 단어 퀴즈 제출 API
-    @PostMapping("/word")
-    @Operation(summary = "단어 퀴즈 제출 API", description = "단어 퀴즈를 풀고 한 문제씩 답을 확인하는 API입니다.")
-    public ApiResult<QuizSubmitRespDto> submitWordQuiz(@RequestBody QuizSubmitReqDto quizSubmitReqDto) {
-        QuizSubmitRespDto quizSubmitRespDto = quizService.submitWordQuiz(quizSubmitReqDto);
-        return success(quizSubmitRespDto);
     }
 
     // 문장 퀴즈 출제 API
@@ -46,6 +40,14 @@ public class QuizController {
         return success(quizService.questionSentenceQuiz());
     }
 
+    // 퀴즈 제출 API
+    @PostMapping
+    @Operation(summary = "퀴즈 제출", description = "퀴즈를 풀고 한 문제씩 답을 확인하는 API입니다.")
+    public ApiResult<QuizSubmitRespDto> submitWordQuiz(@RequestBody QuizSubmitReqDto quizSubmitReqDto) {
+        QuizSubmitRespDto quizSubmitRespDto = quizService.submitWordQuiz(quizSubmitReqDto);
+        return success(quizSubmitRespDto);
+    }
+
     // 퀴즈 완료 API
     @PutMapping("/finish")
     @Operation(summary = "퀴즈를 완료하면 보내는 API")
@@ -53,7 +55,4 @@ public class QuizController {
         Long memberId = jwtTokenProvider.extractMemberId(header.substring(7));
         return success(quizService.finishQuiz(memberId, quizFinishReqDto));
     }
-
-
-
 }
