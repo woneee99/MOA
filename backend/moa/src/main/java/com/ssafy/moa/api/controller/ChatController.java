8abf05ee1e8bdc1bf0b1f1f9d985853f6228fb76
황주원 <woneee99@naver.com>
2023-09-23 14:45:36 +0900

    Feat: 버디 채팅 cors 수정 #10

--- backend/moa/src/main/java/com/ssafy/moa/api/controller/ChatController.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/controller/ChatController.java
@@ -10,12 +10,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.ssafy.moa.common.utils.ApiUtils.success;
 
+@CrossOrigin
 @RestController
 @RequiredArgsConstructor
 public class ChatController {
