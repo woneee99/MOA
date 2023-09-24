c4566566b41ab2a1e3294b29098c2dd90901980c
이하린 <gmlwls608@gmail.com>
2023-09-25 01:13:36 +0900

    Fix: jenkins error resolve

--- backend/moa/src/main/java/com/ssafy/moa/api/controller/ChatController.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/controller/ChatController.java
@@ -13,9 +13,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.ssafy.moa.common.utils.ApiUtils.success;
 
