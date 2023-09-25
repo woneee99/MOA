545eb97ea7e56c20a8997e2ad5deec120869a86e
hyoinjeong <hyoin.jeong3@gmail.com>
2023-09-25 11:43:09 +0900

    Fix: 헤더 변경 #34

--- backend/moa/src/main/java/com/ssafy/moa/api/controller/KeywordController.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/controller/KeywordController.java
@@ -18,7 +18,7 @@ import static com.ssafy.moa.common.utils.ApiUtils.success;
 @RestController
 @RequestMapping("/keyword")
 @RequiredArgsConstructor
-@Tag(name = "Keyword", description = "키워드 API, 등록 API에 access token 필요")
+@Tag(name = "Keyword", description = "키워드 API, 등록/조회 API에 access token 필요")
 @Slf4j
 public class KeywordController {
     private final KeywordService keywordService;
@@ -26,19 +26,17 @@ public class KeywordController {
 
     @Operation(summary = "키워드 등록", description = "밸런스게임을 등록할 수 있습니다.", tags = { "Keyword Controller" })
     @PostMapping
-    public ApiUtils.ApiResult<Long> createKeyword(/* @RequestHeader("Authorization") String header,*/ @RequestBody List<KeywordReqDto> keywordList){
-//        String token = header.substring(7);
-//        Long memberId = jwtTokenProvider.extractMemberId(token);
-        Long memberId = 14L;
+    public ApiUtils.ApiResult<Long> createKeyword(@RequestHeader("Authorization") String header, @RequestBody List<KeywordReqDto> keywordList){
+        String token = header.substring(7);
+        Long memberId = jwtTokenProvider.extractMemberId(token);
         return success(keywordService.createKeyword(memberId, keywordList));
     }
 
     @Operation(summary = "키워드 조회", description = "내가 등록한 키워드들을 조회 할 수 있습니다.", tags = { "Keyword Controller" })
     @GetMapping
-    public ApiUtils.ApiResult<List<KeywordDto>> getAllKeyword(/* @RequestHeader("Authorization") String header,*/){
-//        String token = header.substring(7);
-//        Long memberId = jwtTokenProvider.extractMemberId(token);
-        Long memberId = 14L;
+    public ApiUtils.ApiResult<List<KeywordDto>> getAllKeyword(@RequestHeader("Authorization") String header){
+        String token = header.substring(7);
+        Long memberId = jwtTokenProvider.extractMemberId(token);
         return success(keywordService.getAllKeyword(memberId));
     }
 
