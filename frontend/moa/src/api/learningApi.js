758762c5937b5e7f2b3fe2ddc1f3f95d5947200b
이하린 <gmlwls608@gmail.com>
2023-09-27 11:23:18 +0900

    Feat: 뉴스 데이터 적용 후 기사 조회 #33

--- frontend/moa/src/api/learningApi.js
+++ frontend/moa/src/api/learningApi.js
@@ -1,10 +1,11 @@
 import { authAxios } from "./authAxios";
+import { bigDataAxios } from "./bigDataAxios";
 import { nonAuthAxios } from "./nonAuthAxios";
 
 export const learningApi = {
 
     // 번역 호출
-    translateText : (translatedText) => {
+    translateText: (translatedText) => {
         const translateEndPoint = 'translation'
 
         const requestData = {
@@ -16,8 +17,14 @@ export const learningApi = {
     },
 
     // 스크랩 여부 확인
-    getIsNewsScrap : (articleOriginId) => authAxios.get(`scrap/news/check/${articleOriginId}`),
+    getIsNewsScrap: (articleOriginId) => authAxios.get(`scrap/news/check/${articleOriginId}`),
 
     // 스크랩 등록
-    createNewsScrap : (data) => authAxios.post('scrap/news', data),
+    createNewsScrap: (data) => authAxios.post('scrap/news', data),
+
+    // // 스크랩 삭제
+    // deleteNewsScrap: authAxios.delete('scrap/')
+
+    // 뉴스 기사 조회
+    getNews: (articleId) => bigDataAxios.get(`articles/${articleId}`)
 }
