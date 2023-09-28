547d5a60e6d1963f7922835b0cd725fb3a577733
박진희 <wlsl0620@naver.com>
2023-09-28 22:35:20 +0900

    Design : 단어 퀴즈 화면 css #30

--- frontend/moa/src/components/Quiz/QuestionArea.jsx
+++ frontend/moa/src/components/Quiz/QuestionArea.jsx
@@ -3,6 +3,7 @@ import { quizApi } from '../../api/quizApi';
 import { useNavigate } from 'react-router-dom';
 import Modal from 'react-bootstrap/Modal';
 import MenuHeader from '../MenuHeader';
+import styles from '../../styles/Quiz/WordQuiz.module.css'
 
 function QuestionArea(props) {
   const [quizData, setQuizData] = useState([]);
@@ -149,18 +150,28 @@ function QuestionArea(props) {
           </h1>
           {currentQuiz.quizCategoryId === 2 ? (
             <div>
-              <p>다음 단어를 듣고 맞춰보세요</p>
-              <button onClick={toggleListening}>
-                {isListening ? "듣기 중지" : "듣기"}
-              </button>
+              <p className={styles.quizTitle}>다음 단어를 듣고 맞춰보세요</p>
+              <div className={styles.questionContainer}>
+
+                <div onClick={toggleListening}
+                className={styles.questionArea}>
+                  <img src={process.env.PUBLIC_URL + '/assets/Quiz/quizSound.png'} 
+                  alt="듣기" /> 
+                  {/* <p>{isListening ? "듣기 중지" : "듣기"}</p> */}
+                </div>
+              </div>
             </div>
           ) : (
             <div>
-              <p>다음 단어의 뜻을 맞춰보세요</p>
-              <h1>{currentQuiz.quizQuestion}</h1> 
+              <p className={styles.quizTitle}>다음 단어의 뜻을 맞춰보세요</p>
+              <div className={styles.questionContainer}>
+                <div className={styles.questionArea}>
+                  <p>{currentQuiz.quizQuestion}</p>
+                </div>
+              </div>
             </div>
           )}
-          <ul>
+          <ul className={styles.quizUl}>
             {currentQuiz.quizAnswerList.map((answer,answerIndex) =>(
               <button 
                 key = {answerIndex}
@@ -168,6 +179,7 @@ function QuestionArea(props) {
                   checkAnswer(answer);
                 }}
                 disabled={isCorrect !== null}
+                className={styles.selectBtn}
               >
                 {answer}
               </button>
