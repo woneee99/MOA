d1f40ac436a1f2a3b989eb8b4808ca1d2b11780f
박진희 <wlsl0620@naver.com>
2023-09-28 20:16:34 +0900

    Design : 퀴즈 초기 페이지 css #30

--- frontend/moa/src/pages/Quiz/Quiz.jsx
+++ frontend/moa/src/pages/Quiz/Quiz.jsx
@@ -25,16 +25,40 @@ function Quiz(props) {
         </div>
         <div>
           <Link to="/quiz/question-page">
-            <img src={process.env.PUBLIC_URL + '/assets/Quiz/wordCard.png'} alt="단어퀴즈" /> 
-            {/* <button>퀴즈풀기</button> */}
+            <div className={styles.quizCardContainer}>
+              <img src={process.env.PUBLIC_URL + '/assets/Quiz/wordCard.png'} alt="단어퀴즈" /> 
+              <div className={styles.quizCardText}>
+                <p className={styles.cardMainFont}>단어퀴즈</p>
+                <p>뜻 해석하기, 듣고 맞추기</p>
+              </div>
+            </div>
           </Link>
+
           <Link to="/quiz/incorrect-note">
-            <img src={process.env.PUBLIC_URL + '/assets/Quiz/sentenceCard.png'} alt="문장퀴즈" /> 
-            {/* <button>오답노트</button> */}
+            <div className={styles.quizCardContainer}>
+              <img src={process.env.PUBLIC_URL + '/assets/Quiz/sentenceCard.png'} alt="문장퀴즈" /> 
+              <div className={styles.quizCardText}>
+                <p className={styles.cardMainFont}>문장퀴즈</p>
+                <p>문장 완성하기, 듣고 맞추기</p>
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div className={styles.textArea}>
+          <p className={styles.titleFont}>다시 풀어보기</p>
+          <p>틀린 문제를 다시 풀고 실력을 높여 보세요!</p>
+        </div>
+        <div>
+          <Link to="/quiz/incorrect-note">
+            <div className={styles.quizThird}>
+              <p className={styles.cardMainFont}>다시풀기</p>
+              <p>원하는 만큼만 풀어보세요</p>
+            </div>
           </Link>
         </div>
       </div>
-
+    
     </div>
   );
 }
