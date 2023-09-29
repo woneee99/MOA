23dd8aefdc631f14dabbfb9e2b86ad2b3d96b306
박진희 <wlsl0620@naver.com>
2023-09-29 11:26:03 +0900

    Design : 퀴즈 결과 창 css #30

--- frontend/moa/src/pages/Quiz/QuizResult.jsx
+++ frontend/moa/src/pages/Quiz/QuizResult.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import styles from '../../styles/Quiz/QuizResult.module.css';
 
 function QuizResult(props) {
   // const location = props.location;
@@ -10,7 +11,29 @@ function QuizResult(props) {
 
   return (
     <div>
-      <p>퀴즈 결과 {state}</p>
+      <img 
+       src={process.env.PUBLIC_URL + '/assets/Quiz/resultBg.png'} 
+       alt="결과화면"
+       className={styles.resultBg} 
+      />
+      <div className={styles.quizResultContainer}>
+        <p className={styles.resultTitle}>결과</p>
+        <img 
+          src={process.env.PUBLIC_URL + '/assets/Quiz/resultImg.png'} 
+          alt="결과아이콘"
+          className={styles.resultImg} 
+        />
+        <div className={styles.resultText}>
+          <p>총 {state}문제를</p>
+          <p>맞혔습니다</p>
+        </div>
+
+        <div className={styles.resultBtn}>
+          <p>다른문제풀기</p>
+          <p>돌아가기</p>
+        </div>
+      </div>
+
     </div>
   );
 }
