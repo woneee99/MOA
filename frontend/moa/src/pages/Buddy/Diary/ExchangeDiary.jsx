c33c5d9e6990fffcac7864fde25cc2e77bd65144
이하린 <gmlwls608@gmail.com>
2023-09-30 01:59:15 +0900

    Feat: 교환일기 전체 조회 메인 화면 구성 #37

--- frontend/moa/src/pages/Buddy/Diary/ExchangeDiary.jsx
+++ frontend/moa/src/pages/Buddy/Diary/ExchangeDiary.jsx
@@ -15,28 +15,28 @@ import styles from './ExchangeDiary.module.css'
 import AppBar from '../../../components/AppBar';
 
 function ExchangeDiary(props) {
-  const [diaries, setDiaries] = useState([]);
+  // const [diaries, setDiaries] = useState([]);
 
-  useEffect(() => {
-    diaryApi.getDiaryList()
-      .then((response) => {
-        console.log(response.data);
-        setDiaries(response.data.response);
-      })
-      .catch((e) => {
-        const error = e.error;
-        console.log('교환일기 전체 조회 에러 발생');
-        console.log(e);
-      })
-  }, []);
+  // useEffect(() => {
+  //   diaryApi.getDiaryList()
+  //     .then((response) => {
+  //       console.log(response.data);
+  //       setDiaries(response.data.response);
+  //     })
+  //     .catch((e) => {
+  //       const error = e.error;
+  //       console.log('교환일기 전체 조회 에러 발생');
+  //       console.log(e);
+  //     })
+  // }, []);
 
   const navigate = useNavigate();
 
-  const handleDiaryClick = (exchangeDiaryId) => {
-    navigate(`/buddy/exchangediary/${exchangeDiaryId}`, {
-      state: { exchangeDiaryId }, // 다이어리 데이터를 state에 전달
-    });
-  };
+  // const handleDiaryClick = (exchangeDiaryId) => {
+  //   navigate(`/buddy/exchangediary/${exchangeDiaryId}`, {
+  //     state: { exchangeDiaryId }, // 다이어리 데이터를 state에 전달
+  //   });
+  // };
 
   return (
     <div className={styles.container}>
@@ -55,7 +55,7 @@ function ExchangeDiary(props) {
       </div>
 
       <div>
-        <Link to="/buddy/exchangediary/create" className={`${styles.button} ${styles.button_view}`}>
+        <Link to="/buddy/exchangediary/content" className={`${styles.button} ${styles.button_view}`}>
           일기 보기
         </Link>
       </div>
---
+++ frontend/moa/src/pages/Buddy/Diary/ExchangeDiaryContent.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { diaryApi } from '../../../api/diaryApi';
+import { useNavigate } from 'react-router-dom';
+import MenuHeader from '../../../components/MenuHeader';
+import styles from '../Diary/ExchangeDiaryContent.module.css'
+import Calendar from 'react-calendar';
+import '../Diary/DiaryCalender.css';
+// import 'react-calendar/dist/Calendar.css'; // css import
+import moment from 'moment/moment';
+
+function ExchangeDiaryContent() {
+    const [value, onChange] = useState(new Date());
+    const [exchangeDiaryContent, setExchangeDiaryContent] = useState('');
+
+    const mark = ["2023-09-02", "2023-09-14", "2023-09-27"];
+    const foreignerMark = ["2023-09-14", "2023-09-26"];
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <div className={styles.container}>
+                <MenuHeader title="나의 일기"></MenuHeader>
+                <div className={styles.diaryInside}>
+                    <img
+                        className={styles.diaryInsideImg}
+                        src={process.env.PUBLIC_URL + '/assets/ExchangeDiary/diary_inside.png'}></img>
+                    <div className={styles.diaryCalender}>
+                        <Calendar onChange={onChange}
+                            value={value}
+                            formatDay={(locale, date) => moment(date).format("DD")}
+                            showNeighboringMonth={false}
+                            navigationLabel={null}
+                            next2Label={null}
+                            prev2Label={null}
+                            tileContent={({ date, view }) => {
+                                // 추가할 html 태그를 변수 초기화
+                                let html = [];
+                                // 현재 날짜가 post 작성한 날짜 배열(mark)에 있다면, dot div 추가
+                                if (mark.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
+                                    html.push(<div className="dot"></div>);
+                                }
+                                if (foreignerMark.find((x) => x === moment(date).format("YYYY-MM-DD"))) {
+                                    html.push(<div className='foreignerDot'></div>)
+                                }
+                                // 다른 조건을 주어서 html.push 에 추가적인 html 태그를 적용할 수 있음.
+                                return (
+                                    <>
+                                        <div className="dotDiv">
+                                            {html}
+                                        </div>
+                                    </>
+                                );
+                            }}
+                        ></Calendar>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ExchangeDiaryContent;
