790faabd3638f80a938e189c4433e9c713131720
이하린 <gmlwls608@gmail.com>
2023-09-30 15:02:14 +0900

    Chore: 교환일기 월별 조회 외국인 여부 출력 #37

--- backend/moa/src/main/java/com/ssafy/moa/api/dto/member/MemberDto.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/dto/member/MemberDto.java
@@ -12,11 +12,13 @@ public class MemberDto {
     private Long memberId;
     private String memberName;
     private String memberEmail;
+    private Boolean memberIsForeigner;
 
     @Builder
     public MemberDto(Member member) {
         this.memberId = member.getMemberId();
         this.memberEmail = member.getMemberEmail();
         this.memberName = member.getMemberName();
+        this.memberIsForeigner = member.getMemberIsForeigner();
     }
 }
