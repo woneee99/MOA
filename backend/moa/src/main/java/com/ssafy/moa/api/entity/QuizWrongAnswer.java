850e4f2c6a78535e0147bdaa92c8ebd5be137955
이하린 <gmlwls608@gmail.com>
2023-10-02 12:00:50 +0900

    Refactor: 퀴즈 오답 중복 처리 #18

--- backend/moa/src/main/java/com/ssafy/moa/api/entity/QuizWrongAnswer.java
+++ backend/moa/src/main/java/com/ssafy/moa/api/entity/QuizWrongAnswer.java
@@ -32,4 +32,12 @@ public class QuizWrongAnswer {
         this.quiz = quiz;
     }
 
+    @Override
+    public String toString() {
+        return "QuizWrongAnswer{" +
+                "wrongQuizAnswerId=" + wrongQuizAnswerId +
+                ", member=" + member +
+                ", quiz=" + quiz +
+                '}';
+    }
 }
