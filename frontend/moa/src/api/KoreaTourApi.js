019243d3cf84ccbc3acce1d6d5972e6477af622a
hyoinjeong <hyoin.jeong3@gmail.com>
2023-10-04 14:47:10 +0900

    Fix: 자동완성 API 수정

--- frontend/moa/src/api/KoreaTourApi.js
+++ frontend/moa/src/api/KoreaTourApi.js
@@ -3,5 +3,5 @@ import { nonAuthAxios } from "./nonAuthAxios"
 export const koreaTourApi = {
     // 컨텐츠 검색
   getMediaList: (type, mediaName) => nonAuthAxios.get(`media/search?type=${type}&mediaName=${mediaName}`),
-  getAutoComplete: (mediaName) => nonAuthAxios.get(`media/search?type=all&mediaName=${mediaName}`),
+  getAutoComplete: (mediaName) => nonAuthAxios.get(`media/auto-complete?mediaName=${mediaName}`),
   }
