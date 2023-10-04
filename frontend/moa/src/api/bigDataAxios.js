ea5b6549429fe756af67dd18b89ce50f03f3e673
이하린 <gmlwls608@gmail.com>
2023-10-04 22:20:12 +0900

    Fix: 뉴스 데이터 호출 주소 변경

--- frontend/moa/src/api/bigDataAxios.js
+++ frontend/moa/src/api/bigDataAxios.js
@@ -1,5 +1,5 @@
 import baseAxios from 'axios';
 
 export const bigDataAxios = baseAxios.create({
-    baseURL: "http://j9e204a.p.ssafy.io:8000/",
+    baseURL: "https://j9e204a.p.ssafy.io:8000/",
 });
